c0ada82d02e79d06b7bd4578c5a37ac976a7c9b4
Pranav Tripathi <tripathipranav14@gmail.com>
2025-10-07 10:20:55 +0530

    chore: update GitHub Actions workflow and Svelte config for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,13 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
 	kit: {
 		adapter: adapter({
-			fallback: '404.html' // for client-side routing on Pages
+			fallback: '404.html'
 		}),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/no-time' : ''
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	}
 };
