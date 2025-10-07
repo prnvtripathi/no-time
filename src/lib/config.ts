4ede680fc42db843603c051925f1d6bbfeb7060b
Pranav Tripathi <tripathipranav14@gmail.com>
2025-10-07 10:03:07 +0530

    chore: update workflow configurations to commit logs to reports branch

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,4 @@
 export const GITHUB_OWNER = "prnvtripathi";
 export const GITHUB_REPO = "no-time";
-export const GITHUB_BRANCH = "main";
+export const GITHUB_BRANCH = "reports";
 export const RAW_BASE = `https://raw.githubusercontent.com/${GITHUB_OWNER}/${GITHUB_REPO}/${GITHUB_BRANCH}`;
